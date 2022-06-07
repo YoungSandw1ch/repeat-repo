2217b3fe276f3f5aec256d9ed022802b9145fcc9
YoungSandw1ch <sersus1991@gmail.com>
2022-06-07 20:30:32 +0000

    Deploying to gh-pages from @ YoungSandw1ch/repeat-repo@fe01e483fbf8ca0be7e1c2d2bf59f4d01733b58f 🚀

---
+++ portfolio.452462c2.js
@@ -0,0 +1,2 @@
+var pageId=document.querySelector("[data-id-page]").getAttribute("data-id-page"),menuItem=document.querySelector("[data-id-menu=".concat(pageId,"]"));pageId==menuItem.getAttribute("data-id-menu")&&menuItem.classList.add("menu__link--active");
+//# sourceMappingURL=portfolio.452462c2.js.map
